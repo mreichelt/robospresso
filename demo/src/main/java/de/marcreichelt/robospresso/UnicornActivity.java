bb74ab50e0e5584c6cf2c54d0455f6f580dca6cd
Marc Reichelt <mcreichelt@gmail.com>
2016-05-16 11:13:58 -0700

    added new android project with sample activity

---
+++ demo/src/main/java/de/marcreichelt/robospresso/UnicornActivity.java
@@ -0,0 +1,14 @@
+package de.marcreichelt.robospresso;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UnicornActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unicorn);
+    }
+
+}
---
+++ demo/src/test/java/de/marcreichelt/robospresso/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package de.marcreichelt.robospresso;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
